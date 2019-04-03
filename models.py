3dfb253f9bd8cd094d83cecd49d6af21164c300b
Eugene <supernovaprotocol@gmail.com>
2019-04-03 13:37:27 +0700

    Remove unused code

--- models.py
+++ models.py
@@ -3,145 +3,40 @@ import json
 import numpy as np
 
 
-class DigitsNet:
-    def __init__(self, input_size, hidden_size, output_size):
-        self.initialize(input_size, hidden_size, output_size)
-
-    def initialize(self, input_size, hidden_size, output_size):
-        self.W1 = np.random.randn(hidden_size, input_size) * 0.01
-        self.b1 = np.zeros((hidden_size, 1))
-
-        self.W2 = np.random.randn(output_size, hidden_size) * 0.01
-        self.b2 = np.zeros((output_size, 1))
-
-    def train(self, X, Y, learning_rate, epochs):
-        for i in range(epochs):
-            A2, cache = self.forward_propagation(X)
-            gradients = self.back_propagation(X, A2, Y, cache)
-            dW1 = gradients['dW1']
-            db1 = gradients['db1']
-            dW2 = gradients['dW2']
-            db2 = gradients['db2']
-            self.W1 -= learning_rate * dW1
-            self.b1 -= learning_rate * db1
-            self.W2 -= learning_rate * dW2
-            self.b2 -= learning_rate * db2
-            print('Cost {}'.format(self.compute_cost(Y, A2)))
-
-    def back_propagation(self, X, A2, Y, cache):
-        m = Y.shape[1]
-
-        Z1 = cache['Z1']
-        A1 = cache['A1']
-
-        dZ2 = A2 - Y
-        dW2 = 1.0 / m * np.dot(dZ2, A1.T)
-        db2 = 1.0 / m * np.sum(dZ2, axis=1, keepdims=True)
-
-        dZ1 = np.dot(self.W2.T, dZ2) * self.gprime(Z1)
-        dW1 = 1.0 / m * np.dot(dZ1, X.T)
-        db1 = 1.0 / m * np.sum(dZ1, axis=1, keepdims=True)
-
-        return {
-            'dW2': dW2,
-            'db2': db2,
-            'dW1': dW1,
-            'db1': db1
-        }
-
-    def forward_propagation(self, X):
-        Z1 = np.dot(self.W1, X) + self.b1
-        A1 = np.tanh(Z1)
-
-        Z2 = np.dot(self.W2, A1) + self.b2
-        A2 = self.sigmoid(Z2)
-
-        cache = {
-            'Z1': Z1,
-            'A1': A1
-        }
-        return A2, cache
-
-    def compute_cost(self, Y, Y_hat):
-        m = Y.shape[1]
-        return - np.sum(Y * np.log(Y_hat) + (1 - Y) * np.log(1 - Y_hat)) / m
-
-    def gprime(self, Z):
-        g = np.tanh(Z)
-        return 1 - np.power(g, 2)
-
-    def sigmoid(self, Z):
-        return 1.0 / (1 + np.exp(-Z))
-
-    def softmax(self, Z):
-        a = np.exp(Z)
-        return a / np.sum(a, axis=0, keepdims=True)
-
-    def predict_all(self, X):
-        A2, cache = self.forward_propagation(X)
-        return np.argmax(A2, axis=0)
-
-    def predict(self, x):
-        x = x.reshape(28**2, 1)
-        A2, cache = self.forward_propagation(x)
-        p = np.max(A2)
-        return np.argmax(A2), p
-
-    def save(self, fname):
-        with open(fname, 'w') as f:
-            d = {
-                'W2': self.W2.tolist(),
-                'b2': self.b2.tolist(),
-                'W1': self.W1.tolist(),
-                'b1': self.b1.tolist()
-            }
-
-            f.write(json.dumps(d))
-
-    def restore(self, d):
-        self.W2 = d['W2']
-        self.b2 = d['b2']
-        self.W1 = d['W1']
-        self.b1 = d['b1']
-
-
 def get_model():
     return get_keras_model()
-    if not os.path.isfile('mnist_model.json'):
-        raise Exception('Model file not found')
-
-    with open('mnist_model.json') as f:
-        s = f.read()
-    d = json.loads(s)
-
-    net = DigitsNet(28*28, 30, 10)
-    net.restore(d)
-    return net
 
 
 def initialize_keras_model():
-    from keras.layers import Dense, Dropout
+    from keras.layers import Dense, Dropout, Flatten, BatchNormalization
     from keras.models import Sequential
 
-    drop_prob = 0.15
+    drop_prob = 0.1
 
     model = Sequential()
-    model.add(Dense(units=500, activation='relu', input_dim=28**2))
+    model.add(Flatten(input_shape=(28, 28, 1)))
+    model.add(Dense(units=500, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(units=400, activation='relu'))
+    model.add(Dense(units=400, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(units=300, activation='relu'))
+    model.add(Dense(units=300, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(units=200, activation='relu'))
+    model.add(Dense(units=200, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(units=100, activation='relu'))
+    model.add(Dense(units=100, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
-    model.add(Dense(units=60, activation='relu'))
+    model.add(Dense(units=60, activation='relu', kernel_initializer='he_normal'))
+    model.add(BatchNormalization())
     model.add(Dropout(drop_prob))
 
     model.add(Dense(units=10, activation='softmax'))
@@ -165,45 +60,15 @@ def get_keras_model():
     return Predictor()
 
 
-def estimate_accuracy(net, X, labels):
-    Y_hat = net.predict_all(X)
-
-    labels = np.array(labels)
-    diff = np.round(np.abs(Y_hat - labels))
-    misclass = np.sum(diff > 0)
-
-    mtest = X.shape[1]
-
-    print(
-        'Accurcay {} %'.format((mtest - misclass) / float(mtest) * 100)
-    )
-
-
-def extract_dataset_mean(Xtrain):
-    with open('mnist_info.json', 'w') as f:
-        nx, m = Xtrain.shape
-        mu = np.mean(Xtrain, axis=1, keepdims=True)
-        d = {
-            'mu': mu.tolist()
-        }
-        f.write(json.dumps(d))
-
-
-def normalize(X):
-    if not os.path.isfile('mnist_info.json'):
-        raise Exception('File {} does not exist'.format('mnist_info.json'))
-
-    with open('mnist_info.json', 'r') as f:
-        s = f.read()
-        d = json.loads(s)
-        mu = np.array(d['mu'])
-
-    return (X - mu) / 255.0
+def norm_generator(gen):
+    for x_batch, y_batch in gen:
+        yield x_batch / 255.0, y_batch
 
 
 def train_keras_model(learning_rate=0.001, epochs=10):
     from mnist import MNIST
     import keras
+    from util import augmented_dataset_generator
 
     mndata = MNIST('./datasets/mnist')
     images, labels = mndata.load_training()
@@ -211,77 +76,41 @@ def train_keras_model(learning_rate=0.001, epochs=10):
 
     m_train = len(images)
     m_test = len(images_test)
-    #m_train = 10000
-    #m_test = 10000
+    batch_size = 128
 
     images = images[:m_train]
     labels = labels[:m_train]
     images_test = images_test[:m_test]
     labels_test = labels_test[:m_test]
 
-    Xtrain = np.array(images, dtype=np.uint8)
-    Xtest = np.array(images_test, dtype=np.uint8)
+    gen = augmented_dataset_generator(images, labels, batch_size)
+    Xtest = np.array(images_test, dtype=np.uint8).reshape((m_test, 28, 28, 1)) / 255.0
 
-    Xtrain_norm = Xtrain / 255.0
-    Xtest_norm = Xtest / 255.0
-
-    Ytrain = np.array(labels, dtype=np.uint8).reshape(m_train, 1)
     Ytest = np.array(labels_test, dtype=np.uint8).reshape(m_test, 1)
-
-    Ytrain = keras.utils.to_categorical(Ytrain, num_classes=10)
-    Ytest = keras.utils.to_categorical(Ytest, num_classes=10)
 
     model = initialize_keras_model()
 
-    optimizer = keras.optimizers.SGD(
-        lr=learning_rate, momentum=0.9, decay=0.0, nesterov=False
+    optimizer = keras.optimizers.Adam(
+        lr=learning_rate
     )
     model.compile(loss='categorical_crossentropy',
                   optimizer=optimizer,
                   metrics=['accuracy'])
 
-    model.fit(Xtrain_norm, Ytrain, epochs=epochs, batch_size=32)
+    from math import ceil
 
-    loss_and_metrics = model.evaluate(Xtest_norm, Ytest, batch_size=128)
+    model.fit_generator(norm_generator(gen), epochs=epochs, steps_per_epoch=ceil(m_train / batch_size))
+
+    gen = norm_generator(augmented_dataset_generator(images_test, labels_test, batch_size))
+
+    loss_and_metrics = model.evaluate_generator(gen, steps=ceil(m_test / batch_size))
+    print(loss_and_metrics)
+    loss_and_metrics = model.evaluate(
+        Xtest,
+        keras.utils.to_categorical(Ytest, num_classes=10)
+    )
     print(loss_and_metrics)
     model.save_weights('keras_model.h5')
-
-
-def train_model(learning_rate=0.001, epochs=10):
-    from mnist import MNIST
-    mndata = MNIST('./datasets/mnist')
-    images, labels = mndata.load_training()
-    images_test, labels_test = mndata.load_testing()
-
-    Xtrain = np.array(images, dtype=np.uint8).T
-    Xtest = np.array(images_test, dtype=np.uint8).T
-
-    extract_dataset_mean(Xtrain)
-
-    Xtrain_norm = normalize(Xtrain)
-    Xtest_norm = normalize(Xtest)
-
-    mtrain = Xtrain.shape[1]
-    mtest = Xtest.shape[1]
-
-    Ytrain = np.zeros((10, mtrain), dtype=np.float)
-    Ytest = np.zeros((10, mtest), dtype=np.float)
-
-    for i in range(mtrain):
-        label = labels[i]
-        Ytrain[label, i] = 1.0
-
-    for i in range(mtest):
-        label = labels_test[i]
-        Ytest[label, i] = 1.0
-
-    net = DigitsNet(28*28, 100, 10)
-    net.train(Xtrain_norm, Ytrain, learning_rate=learning_rate, epochs=epochs)
-
-    estimate_accuracy(net, Xtrain_norm, labels)
-    estimate_accuracy(net, Xtest_norm, labels_test)
-
-    net.save('mnist_model.json')
 
 
 if __name__ == '__main__':
@@ -290,7 +119,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Given an image, compress it using JPEG algorithm'
     )
-    parser.add_argument('--lrate', type=float, default=0.003,
+    parser.add_argument('--lrate', type=float, default=0.001,
                         help='learning rate')
 
     parser.add_argument('--epochs', type=int, default=30,
@@ -298,5 +127,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    #train_model(args.lrate, args.epochs)
     train_keras_model(args.lrate, args.epochs)
--- util.py
+++ util.py
@@ -1,186 +1,23 @@
-import os
 import numpy as np
 
 
-dataset_root = './datasets/mnist'
+def augmented_dataset_generator(images, labels, batch_size=32):
+    import keras
+    from keras.preprocessing.image import ImageDataGenerator
 
+    m = len(images)
+    x_train = np.array(images).reshape((m, 28, 28, 1))
+    y_train = np.array(labels)
+    y_train = keras.utils.to_categorical(y_train, num_classes=10)
 
-def shift_pixels_right(image, k):
-    x = np.empty_like(image)
-    x[:, k:28] = image[:, :28-k]
-    x[:, :k] = np.zeros((28, k))
-    return x
+    datagen = ImageDataGenerator(
+        featurewise_center=False,
+        featurewise_std_normalization=False,
+        rotation_range=15,
+        width_shift_range=5,
+        height_shift_range=5,
+        horizontal_flip=False)
 
-
-def shift_pixels_left(image, k):
-    x = np.empty_like(image)
-    x[:, :28-k] = image[:, k:28]
-
-    x[:, 28-k:] = np.zeros((28, k))
-    return x
-
-
-def shift_pixels_up(image, k):
-    x = np.empty_like(image)
-    x[:k, :] = image[-k:, :]
-    x[-k:, :] = np.zeros((k, 28))
-    return x
-
-
-def shift_pixels_down(image, k):
-    x = np.empty_like(image)
-    x[:28-k, :] = image[k:28, :]
-
-    x[28-k:, :] = np.zeros((k, 28))
-    return x
-
-
-def horizontal_shift(image, k):
-    if k > 0:
-        return shift_pixels_right(image, k)
-    else:
-        return shift_pixels_left(image, abs(k))
-
-
-def vertical_shift(image, k):
-    if k > 0:
-        return shift_pixels_down(image, k)
-    else:
-        return shift_pixels_up(image, abs(k))
-
-
-def embed_noise(a, noise=50):
-    assert a.ndim == 2
-    res = np.zeros_like(a, dtype=np.int)
-    res[:, :] = np.minimum(np.random.randn(*a.shape) * noise + a, 255)
-    return np.maximum(res, 0)
-
-
-class CoordinateSystem:
-    def __init__(self, a, x0=0, y0=0):
-        assert a.ndim == 2
-
-        self.a = a
-        self.x0 = x0
-        self.y0 = y0
-
-    def map_to_coordinates(self):
-        a = self.a
-
-        indices = []
-        for i in range(a.shape[0]):
-            for j in range(a.shape[1]):
-                x = j - self.x0
-                y = a.shape[0] - 1 - i
-                y -= self.y0
-                indices.append((x, y))
-
-        return np.array(indices, dtype=a.dtype).T
-
-    def coordinates_to_cell(self, x, y):
-        row = y + self.y0
-        col = x + self.x0
-        row = self.a.shape[0] - 1 - row
-
-        return row, col
-
-
-def within_bounds(size, index):
-    return index >= 0 and index < size
-
-
-def rotate(a, angle, origin=None):
-    assert a.ndim == 2
-    rads = angle * np.pi / 180.0
-
-    if not origin:
-        origin = (a.shape[0] - 1, 0)
-
-    y0 = a.shape[0] - 1 - origin[0]
-    x0 = origin[1]
-
-    system = CoordinateSystem(a, x0, y0)
-
-    cos_phi = np.cos(rads)
-    sin_phi = np.sin(rads)
-    R = np.array([[cos_phi, -sin_phi],
-                  [sin_phi, cos_phi]])
-
-    X = system.map_to_coordinates()
-
-    X_transformed = np.zeros((2, X.shape[1]), dtype=np.int)
-    X_transformed[:, :] = np.round(np.dot(R, X))
-
-    res = np.zeros_like(a)
-    for j in range(X_transformed.shape[1]):
-        pixel_val = a.reshape(a.shape[0] * a.shape[0])[j]
-
-        x = X_transformed[0, j]
-        y = X_transformed[1, j]
-
-        row, col = system.coordinates_to_cell(x, y)
-
-        if within_bounds(a.shape[0], row) and within_bounds(a.shape[1], col):
-            res[row, col] = pixel_val
-
-    return res
-
-
-def scale(image, scaling_factor):
-    return image
-
-
-def random_transformation(pixels):
-    max_shift = 6
-    hor_shift = np.random.randint(-max_shift, max_shift)
-    vert_shift = np.random.randint(-max_shift, max_shift)
-    noise_mag = np.random.randint(15, 30)
-    rotation_angle = np.random.randint(-50, 50)
-    scaling_factor = np.random.randint(1, 3)
-
-    image = np.array(pixels).reshape(28, 28)
-
-    image = rotate(image, rotation_angle, origin=(14, 14))
-
-    if hor_shift != 0:
-        image = horizontal_shift(image, hor_shift)
-
-    if vert_shift != 0:
-        image = vertical_shift(image, vert_shift)
-
-    image = embed_noise(image, noise_mag)
-
-    image = scale(image, scaling_factor)
-
-    return image
-
-
-def list_to_line(elements):
-    return ' '.join([str(x) for x in elements]) + '\n'
-
-
-def line_to_list(line):
-    return [int(num) for num in line.strip().split(' ')]
-
-
-def training_batches(extended_x_path, extended_y_path, batch_size=100):
-    X = []
-    Y = []
-    with open(extended_x_path, 'r') as fx, open(extended_y_path, 'r') as fy:
-        while True:
-            line = fx.readline()
-            if line == '':
-                X_matrix = np.array(X, dtype=np.uint8)
-                Y_matrix = np.array(Y, dtype=np.uint8).reshape(1, len(Y))
-                yield X_matrix.T, Y_matrix
-                return
-            x = line_to_list(line)
-            y = line_to_list(fy.readline())[0]
-            X.append(x)
-            Y.append(y)
-            if len(Y) >= batch_size:
-                X_matrix = np.array(X, dtype=np.uint8)
-                Y_matrix = np.array(Y, dtype=np.uint8).reshape(1, len(Y))
-                yield X_matrix.T, Y_matrix
-                X[:] = []
-                Y[:] = []
+    datagen.fit(x_train)
+    gen = datagen.flow(x_train, y_train, batch_size=batch_size)
+    return gen
