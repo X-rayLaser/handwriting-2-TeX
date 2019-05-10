d40c471506b658f51d59cbd3e53c6ba5cac8bfbd
Eugene <supernovaprotocol@gmail.com>
2019-05-10 20:23:25 +0700

    Yolo data generation script almost complete

--- yolo/create_dataset.py
+++ yolo/create_dataset.py
@@ -15,24 +15,70 @@ class YoloVolume:
         self.grid_size = grid_size
         self.num_classes = num_classes
 
-        self._volume = np.zeros((grid_size, grid_size, self.depth))
+        self._volume = np.zeros((grid_size, grid_size, self.depth), dtype=np.uint8)
 
-    def add_item(self, xy_center, width, height, class_index):
-        xc, yc = xy_center
+        self._boxes = []
 
+    @staticmethod
+    def unroll_classes(flat_volume, grid_size, num_classes):
+        confidences_score_size = 1
+        box_size = 4
+        label_size = 1
+        depth = confidences_score_size + 4 + label_size
+
+        array_3d = flat_volume.reshape((grid_size, grid_size, depth))
+        h, w, d = array_3d.shape
+
+        new_depth = confidences_score_size + box_size + num_classes
+        res = np.zeros((w, h, new_depth))
+
+        for i in range(h):
+            for j in range(w):
+                v = array_3d[i, j, :]
+                label_index = confidences_score_size + box_size
+                label = v[label_index]
+
+                from keras.utils import to_categorical
+
+                class_distribution = to_categorical(label,
+                                                    num_classes=num_classes)
+                res[i, j, :] = np.concatenate((array_3d[i, j, :label_index],
+                                               class_distribution))
+
+        return res
+
+    def detects_collision(self, bounding_box):
+        comparison_box = self._to_shapely_box(bounding_box)
+
+        for box in self._boxes:
+            b = self._to_shapely_box(box)
+            if comparison_box.intersection(b).area > 0:
+                return True
+
+        return False
+
+    def _to_shapely_box(self, bounding_box):
+        from shapely.geometry import box
+
+        xc, yc, width, height = bounding_box
+        x = xc - width // 2
+        y = yc - height // 2
+        return box(x, y, x + width, y + height)
+
+    def add_item(self, bounding_box, class_index):
+        xc, yc, width, height = bounding_box
         col, row = self.position_to_grid_cell(xc, yc)
 
         detection_confidence = 1
 
-        box_vector = self.get_bounding_box(xc, yc, col, row, width=width,
-                                           height=height)
+        #box_vector = self.get_bounding_box(xc, yc, col, row, width=width,
+        #                                   height=height)
 
-        from keras.utils import to_categorical
-
-        one_hot_classes = to_categorical(class_index, num_classes=self.num_classes)
+        box_vector = [xc, yc, width, height]
+        self._boxes.append(bounding_box)
 
         self._volume[row, col] = self.make_prediction_vector(
-            detection_confidence, box_vector, one_hot_classes
+            detection_confidence, box_vector, class_index
         )
 
     def position_to_grid_cell(self, xc, yc):
@@ -57,8 +103,8 @@ class YoloVolume:
 
         return np.array([xrel, yrel, w, h])
 
-    def make_prediction_vector(self, confidence, bounding_box, class_distribution):
-        return np.concatenate(([confidence], bounding_box, class_distribution))
+    def make_prediction_vector(self, confidence, bounding_box, class_index):
+        return np.concatenate(([confidence], bounding_box, [class_index]))
 
     @property
     def output_volume(self):
@@ -68,7 +114,8 @@ class YoloVolume:
     def depth(self):
         confidence_score = 1
         bounding_box_size = 4
-        return confidence_score + bounding_box_size + self.num_classes
+        label_size = 1
+        return confidence_score + bounding_box_size + label_size
 
     @property
     def cell_width(self):
@@ -77,6 +124,10 @@ class YoloVolume:
     @property
     def cell_height(self):
         return self.img_height / self.grid_size
+
+
+class BoundingBoxSuggestionError(Exception):
+    pass
 
 
 class YoloDataGenerator:
@@ -108,14 +159,17 @@ class YoloDataGenerator:
 
         return x, y
 
-    def position_to_grid_cell(self, xc, yc):
-        x = xc // self.cell_width
-        y = yc // self.cell_height
+    def try_choosing_position(self, volume, max_retries=10):
+        for i in range(max_retries):
+            xc, yc = self.choose_global_position()
 
-        return int(x), int(y)
+            width, height = 45, 45
+            bounding_box = (xc, yc, width, height)
 
-    def make_prediction_vector(self, confidence, bounding_box, class_index):
-        return np.concatenate(([confidence], bounding_box, [class_index]))
+            if not volume.detects_collision(bounding_box):
+                return bounding_box
+
+        raise BoundingBoxSuggestionError()
 
     def make_example(self, elements=40):
         canvas = Canvas(self.img_width, self.img_height, self.primitives_dir)
@@ -124,13 +178,18 @@ class YoloDataGenerator:
 
         for _ in range(elements):
             class_index, category = self.choose_category()
-            xc, yc = self.choose_global_position()
 
-            volume.add_item((xc, yc), width=45, height=45, class_index=class_index)
+            try:
+                bounding_box = self.try_choosing_position(volume)
+            except BoundingBoxSuggestionError:
+                continue
+            else:
+                volume.add_item(bounding_box, class_index=class_index)
 
-            x = xc - 45 / 2
-            y = yc - 45 / 2
-            canvas.draw_random_class_image(x, y, category)
+                xc, yc, _, _ = bounding_box
+                x = xc - 45 // 2
+                y = yc - 45 // 2
+                canvas.draw_random_class_image(x, y, category)
 
         input = canvas.image_data
         return input, volume.output_volume
@@ -140,17 +199,15 @@ class YoloDataGenerator:
         for i in range(num_examples):
             elements = random.randint(0, 120)
             input, output = self.make_example(elements=elements)
-
-            visualize_image(input)
             print(output)
 
 
 class YoloDatasetHome:
     def __init__(self, dataset_root):
         self._dataset_root = dataset_root
-        os.makedirs(self.train_path)
-        os.makedirs(self.dev_path)
-        os.makedirs(self.test_path)
+        os.makedirs(self.train_path, exist_ok=True)
+        os.makedirs(self.dev_path, exist_ok=True)
+        os.makedirs(self.test_path, exist_ok=True)
 
     @staticmethod
     def initialize_dataset(dataset_root, num_examples,
@@ -203,18 +260,45 @@ class YoloDatasetHome:
         return os.path.join(self.root_path, 'test')
 
     def get_all_examples(self):
-        input_shape = self.config['input_config']['shape']
-
-        n = 1
-
-        for dim_size in input_shape:
-            n *= dim_size
+        n = get_input_size(self.config)
 
         for row in self.all_rows:
             xy_list = map(np.uint8, row)
             x = xy_list[:n]
             y = xy_list[n:]
             yield x, y
+
+    def flow_with_preload(self, dataset_path, mini_batch_size, normalize=False):
+        preloader = Preloader(dataset_path, self.config)
+        inputs, outputs = preloader.preload()
+
+        m = len(inputs)
+
+        grid_size = self.config['output_config']['grid_size']
+        num_classes = self.config['output_config']['num_classes']
+
+        while True:
+            # todo: shuffling analog (random picking) for without preload case
+            #x, labels = shuffle_data(x, labels)
+
+            for i in range(0, outputs.shape[0], mini_batch_size):
+                x_batch = inputs[i:i + mini_batch_size, :]
+                input_shape = (x_batch.shape[0], ) + tuple(self.config['input_config']['shape'])
+                x_batch = x_batch.reshape(input_shape)
+
+                if normalize:
+                    x_batch = x_batch / 255.0
+
+                y_batch = outputs[i:i + mini_batch_size, :]
+
+                y_batch = [YoloVolume.unroll_classes(y, grid_size, num_classes)
+                           for y in y_batch]
+
+                yield x_batch, np.array(y_batch)
+
+    def flow(self, dataset_path, mini_batch_size, normalize=False):
+        while True:
+            pass
 
     @property
     def all_rows(self):
@@ -235,7 +319,7 @@ class YoloDatasetHome:
         d = {
             'num_examples': num_examples,
             'input_config': input_config,
-            'output_config': output_config
+            'output_config': output_config,
             'training_fraction': training_fraction,
             'number_of_parts': number_of_parts
         }
@@ -282,6 +366,25 @@ class YoloDatasetHome:
             f.write(s)
 
 
+def total_size(shape):
+    n = 1
+
+    for dim_size in shape:
+        n *= dim_size
+
+    return n
+
+
+def get_input_size(config):
+    input_shape = config['input_config']['shape']
+    return total_size(input_shape)
+
+
+def get_output_size(config):
+    output_shape = config['output_config']['shape']
+    return total_size(output_shape)
+
+
 def generate_dataset(primitives_source, destination_dir, num_examples):
     image_width = 350
     image_height = 350
@@ -291,11 +394,19 @@ def generate_dataset(primitives_source, destination_dir, num_examples):
     input_config = {
         'image_width': image_width,
         'image_height': image_height,
+        'shape': (image_width, image_height)
     }
+
+    conf_score_size = 1
+    box_size = 4
+    label_size = 1
+
+    depth = conf_score_size + box_size + label_size
 
     output_config = {
         'grid_size': grid_size,
-        'num_classes': num_classes
+        'num_classes': num_classes,
+        'shape': (grid_size, grid_size, depth)
     }
 
     yolo_home = YoloDatasetHome.initialize_dataset(destination_dir,
@@ -307,7 +418,7 @@ def generate_dataset(primitives_source, destination_dir, num_examples):
                             grid_size=grid_size, num_classes=num_classes)
 
     for i in range(num_examples):
-        n = 4
+        n = 30
         input, output = gen.make_example(elements=n)
         yolo_home.add_example(input, output)
 
@@ -337,8 +448,21 @@ def precompute_features(dataset_root, destination):
 
 
 def load_part(args):
-    path, num_examples, options = args
-    # todo: finish this one
+    path, num_examples, input_size, output_size = args
+
+    dtype = np.uint8
+    x_batch = np.zeros((num_examples, input_size), dtype=dtype)
+    y_batch = np.zeros((num_examples, output_size), dtype=dtype)
+
+    i = 0
+    with open(path, 'r', newline='') as f:
+        for row in csv.reader(f):
+            xy = list(map(np.uint8, row))
+            x_batch[i, :] = xy[:input_size]
+            y_batch[i, :] = xy[input_size:]
+            i += 1
+
+    return x_batch, y_batch
 
 
 class Preloader:
@@ -347,13 +471,42 @@ class Preloader:
         self._config = dataset_config
 
     def preload(self, max_workers=4):
+        m, _ = self._number_of_examples()
+        x = np.zeros((m, self._input_size), dtype=np.uint8)
+        y = np.zeros((m, self._output_size), dtype=np.uint8)
+
+        results = self._parallel_load(max_workers)
+
+        i = 0
+
+        for x_batch, y_batch in results:
+            m = x_batch.shape[0]
+            x[i:i + m, :] = x_batch
+            y[i:i + m] = y_batch
+            i += m
+
+        return x, y
+
+    def _parallel_load(self, max_workers):
+        _, counts = self._number_of_examples()
+
+        arg_list = []
+
+        for fname in os.listdir(self._path):
+            file_path = os.path.join(self._path, fname)
+            num_examples = counts[fname]
+            arg_list.append((file_path, num_examples, self._input_size, self._output_size))
+
         pool = ProcessPoolExecutor(max_workers=max_workers)
+        return list(pool.map(load_part, arg_list))
 
-        arg_list = [(csv_files_dir, fname, category_counts)
-                    for fname in os.listdir(self._path)]
+    @property
+    def _input_size(self):
+        return get_input_size(self._config)
 
-        results = list(pool.map(all_examples_with_category, arg_list))
-        # todo: finish impl
+    @property
+    def _output_size(self):
+        return get_output_size(self._config)
 
     def _number_of_examples(self):
         counts = {}
@@ -364,65 +517,25 @@ class Preloader:
                 for _ in csv.reader(f):
                     counts[fname] += 1
 
-        return counts
-
-
-def load_pretrained_examples(path, num_examples, width, height, channels, grid_size, dtype=np.uint8):
-    m = num_examples
-
-    confidence_score_size = 1
-    bounding_box_size = 4
-    label_size = 1
-    ydepth = confidence_score_size + bounding_box_size + label_size
-
-    x = np.zeros((m, width * height * channels), dtype=dtype)
-    y = np.zeros((m, grid_size * grid_size * ydepth), dtype=np.uint8)
-
-    with open(path, 'r', newline='') as fin:
-        reader = csv.reader(fin, delimiter=',')
-
-        i = 0
-        for row in reader:
-            line = list(map(dtype, row))
-            separator = width * height * channels
-            x[i, :] = line[:separator]
-            y[i, :] = line[separator:]
-            i += 1
-
-    return x, y
-
-
-def preload_raw_data(dataset_dir, width, height, grid_size):
-    counter_file_name = 'counter.json'
-    counter_path = os.path.join(dataset_dir, counter_file_name)
-
-    import json
-
-    with open(counter_path, 'r') as f:
-        s = f.read()
-        data_sizes = json.loads(s)
-
-    m = sum(data_sizes.values())
-
-    x = np.zeros((m, width, height))
-
-    csv_files = [fname for fname in os.listdir(dataset_dir)]
-
-    load_examples(path, num_examples, width, height, channels, grid_size, dtype=np.uint8)
-
-
-def create_dataset(destination_dir):
-    csv_dir = '../datasets/digits_and_operators_csv/train'
-    generate_dataset(primitives_source=csv_dir, destination_dir=destination_dir, num_examples=2)
-    precompute_features(destination_dir)
-    split_data(destination_dir)
+        total_count = sum(counts.values())
+        return total_count, counts
 
 
 if __name__ == '__main__':
     destination_dir = '../datasets/yolo_dataset'
-    create_dataset(destination_dir)
-    preload_data(destination_dir)
-    #csv_dir = '../datasets/digits_and_operators_csv/dev'
+    csv_dir = '../datasets/digits_and_operators_csv/train'
+    generate_dataset(primitives_source=csv_dir, destination_dir=destination_dir, num_examples=5)
 
-    #gen = YoloDataGenerator(350, 250, csv_dir)
-    #gen.generate_dataset(2)
+    train_dir = os.path.join(destination_dir, 'train')
+
+    yolo_home = YoloDatasetHome(destination_dir)
+    counter = 0
+    for x_batch, y_batch in yolo_home.flow_with_preload(train_dir, mini_batch_size=2):
+        x = x_batch[0]
+        y = y_batch[0]
+        visualize_image(x)
+
+        counter += 1
+        print(y_batch.shape)
+        if counter > 2:
+            break
